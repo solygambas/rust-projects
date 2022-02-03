878e9fd6c9b03c36f9b3fd6862969c1a1a6224a9
solygambas <51904909+solygambas@users.noreply.github.com>
2022-02-03 10:08:14 +0100

    add structs

--- playground/hello-cargo/src/main.rs
+++ playground/hello-cargo/src/main.rs
@@ -8,7 +8,8 @@
 // mod conditionals;
 // mod loops;
 // mod functions;
-mod pointer_ref;
+// mod pointer_ref;
+mod structs;
 
 fn main() {
     // println!("Hello World!")
@@ -22,5 +23,6 @@ fn main() {
     // conditionals::run()
     // loops::run()
     // functions::run()
-    pointer_ref::run()
+    // pointer_ref::run()
+    structs::run()
 }
