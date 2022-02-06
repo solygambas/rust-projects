1dd9037ffc2c341df8d1b5341175cff264e418e5
solygambas <51904909+solygambas@users.noreply.github.com>
2022-02-06 08:42:43 +0100

    add guessing game

--- 02-guessing-game/src/main.rs
+++ 02-guessing-game/src/main.rs
@@ -1,11 +1,30 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
-    println!("Please input your guess.");
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-    println!("You guessed: {}", guess);
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    // println!("The secret number is {}", secret_number);
+    loop {
+        println!("Please input your guess.");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        let guess: u32 = match guess.trim().parse() {
+            // Result is an enum that has the variants Ok or Err
+            Ok(num) => num,
+            Err(_) => continue, // ask for another guess
+        };
+        println!("You guessed: {}", guess);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
+    }   
 }
--- README.md
+++ README.md
@@ -37,5 +37,10 @@ A hands-on introduction to the Rust language.
 
 - handling user input with the io library.
 - adding rand from [crates.io](https://crates.io/) as a dependency.
+- generating a secret number.
+- comparing the guess to the secret number.
+- allowing multiple guesses with looping.
+- quitting after a correct guess.
+- handling invalid input.
 
 Based on [The Rust Programming Language](https://doc.rust-lang.org/book/) by Steve Klabnik and Carol Nichols (2021).
