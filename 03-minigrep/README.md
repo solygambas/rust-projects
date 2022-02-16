eabd60ceda24227f4940a916602086589bbaed28
solygambas <51904909+solygambas@users.noreply.github.com>
2022-02-16 14:19:48 +0100

    refactor and add error handling

--- 03-minigrep/README.md
+++ 03-minigrep/README.md
@@ -6,5 +6,6 @@ A simple version of the classic command line tool grep.
 
 - accepting command line arguments.
 - reading a file.
+- refactoring to improve modularity and error handling.
 
 Based on [The Rust Programming Language](https://doc.rust-lang.org/book/) by Steve Klabnik and Carol Nichols (2021).
--- 03-minigrep/src/main.rs
+++ 03-minigrep/src/main.rs
@@ -1,14 +1,27 @@
 use std::env;
-use std::fs;
+use std::process;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     // println!("{:?}", args); // ["target\\debug\\minigrep.exe", "needle", "haystack"]
-    let query = &args[1];
-    let filename = &args[2];
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    // let query = &args[1];
+    // let filename = &args[2];
 
-    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
-    println!("With text:\n{}", contents);
+    // if args.len() < 3 {
+    //     panic!("not enough arguments");
+    // }
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
