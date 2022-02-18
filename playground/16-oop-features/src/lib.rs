f88c5a52c67401450c2304be34020a957e411dea
solygambas <51904909+solygambas@users.noreply.github.com>
2022-02-18 12:25:37 +0100

    add object-oriented design pattern

--- playground/16-oop-features/src/lib.rs
+++ playground/16-oop-features/src/lib.rs
@@ -1,27 +1,112 @@
-pub trait Draw {
-    fn draw(&self);
+pub struct Post {
+    // state: Option<Box<dyn State>>,
+    content: String,
 }
 
-pub struct Screen {
-    pub components: Vec<Box<dyn Draw>>,
+pub struct DraftPost {
+    content: String,
 }
 
-impl Screen {
-    pub fn run(&self) {
-        for component in self.components.iter() {
-            component.draw();
+impl Post {
+    // pub fn new() -> Post {
+    //     Post {
+    //         state: Some(Box::new(Draft {})),
+    //         content: String::new(),
+    //     }
+    // }
+
+    // pub fn add_text(&mut self, text: &str) {
+    //     self.content.push_str(text);
+    // }
+
+    pub fn new() -> DraftPost {
+        DraftPost {
+            content: String::new(),
+        }
+    }
+
+    pub fn content(&self) -> &str {
+        // self.state.as_ref().unwrap().content(self)
+        &self.content
+    }
+
+    // pub fn request_review(&mut self) {
+    //     if let Some(s) = self.state.take() {
+    //         self.state = Some(s.request_review())
+    //     }
+    // }
+
+    // pub fn approve(&mut self) {
+    //     if let Some(s) = self.state.take() {
+    //         self.state = Some(s.approve())
+    //     }
+    // }
+}
+
+impl DraftPost {
+    pub fn add_text(&mut self, text: &str) {
+        self.content.push_str(text);
+    }
+
+    pub fn request_review(self) -> PendingReviewPost {
+        PendingReviewPost {
+            content: self.content,
         }
     }
 }
 
-pub struct Button {
-    pub width: u32,
-    pub height: u32,
-    pub label: String,
+pub struct PendingReviewPost {
+    content: String,
 }
 
-impl Draw for Button {
-    fn draw(&self) {
-        // code to draw a button
+impl PendingReviewPost {
+    pub fn approve(self) -> Post {
+        Post {
+            content: self.content,
+        }
+    }
+}
+
+trait State {
+    fn request_review(self: Box<Self>) -> Box<dyn State>;
+    fn approve(self: Box<Self>) -> Box<dyn State>;
+    fn content<'a>(&self, post: &'a Post) -> &'a str {
+        ""
+    }
+}
+
+struct Draft {}
+
+impl State for Draft {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        Box::new(PendingReview {})
+    }
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+}
+
+struct PendingReview {}
+
+impl State for PendingReview {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        Box::new(Published {})
+    }
+}
+
+struct Published {}
+
+impl State for Published {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+    fn content<'a>(&self, post: &'a Post) -> &'a str {
+        &post.content
     }
 }
--- playground/16-oop-features/src/main.rs
+++ playground/16-oop-features/src/main.rs
@@ -1,36 +1,15 @@
-use oop::Draw;
-use oop::{Button, Screen};
-
-struct SelectBox {
-    width: u32,
-    height: u32,
-    options: Vec<String>,
-}
-
-impl Draw for SelectBox {
-    fn draw(&self) {
-        // code to draw a select box
-    }
-}
+use oop::Post;
 
 fn main() {
-    let screen = Screen {
-        compoents: vec![
-            Box::new(SelectBox {
-                width: 75,
-                height: 10,
-                options: vec![
-                    String::from("Yes"),
-                    String::from("Maybe"),
-                    String::from("No"),
-                ],
-            }),
-            Box::new(Button {
-                width: 50,
-                height: 10,
-                label: String::from("OK"),
-            }),
-        ],
-    };
-    screen.run();
+    let mut post = Post::new();
+
+    post.add_text("I ate a salad for lunch today");
+    // assert_eq!("", post.content());
+
+    let post = post.request_review();
+    // assert_eq!("", post.content());
+
+    let post = post.approve();
+    assert_eq!("I ate a salad for lunch today", post.content());
+    // println!("{}", post.content());
 }
