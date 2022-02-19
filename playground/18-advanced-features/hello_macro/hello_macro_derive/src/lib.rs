20a1bbca60689befb366dd5d4a111ab459db8d6c
solygambas <51904909+solygambas@users.noreply.github.com>
2022-02-19 12:22:11 +0100

    add macros

---
+++ playground/18-advanced-features/hello_macro/hello_macro_derive/src/lib.rs
@@ -0,0 +1,26 @@
+extern crate proc_macro; // allows us to read and manipulate Rust code from our code
+
+use proc_macro::TokenStream;
+use quote::quote; // turns syn data structures back into Rust code.
+use syn; // parses Rust code from a string into a data structure that we can perform operations on
+
+#[proc_macro_derive(HelloMacro)]
+pub fn hello_macro_derive(input: TokenStream) -> TokenStream {
+    // Construct a representation of Rust code as a syntax tree
+    // that we can manipulate
+    let ast = syn::parse(input).unwrap();
+    // Build the trait implementation
+    impl_hello_macro(&ast)
+}
+
+fn impl_hello_macro(ast: &syn::DeriveInput) -> TokenStream {
+    let name = &ast.ident;
+    let gen = quote! {
+        impl HelloMacro for #name {
+            fn hello_macro() {
+                println!("Hello, Macro! My name is {}!", stringify!(#name));
+            }
+        }
+    };
+    gen.into()
+}
