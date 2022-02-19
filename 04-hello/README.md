270d79061ec19a6d751bb06875fb58f56cc0fd4f
solygambas <51904909+solygambas@users.noreply.github.com>
2022-02-19 15:02:07 +0100

    add multithreaded server

--- 04-hello/README.md
+++ 04-hello/README.md
@@ -7,5 +7,7 @@ A multithreaded web server that says “hello”.
 - listening to the TCP connection and reading the request.
 - writing a response and returning HTML.
 - validating the request and selectively responding.
+- simulating a slow request.
+- improving throughput with a thread pool.
 
 Based on [The Rust Programming Language](https://doc.rust-lang.org/book/) by Steve Klabnik and Carol Nichols (2021).
--- 04-hello/src/bin/main.rs
+++ 04-hello/src/bin/main.rs
@@ -2,13 +2,21 @@ use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
+use std::thread;
+use std::time::Duration;
+
+use hello::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
+        // handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
@@ -19,8 +27,13 @@ fn handle_connection(mut stream: TcpStream) {
     // let response = "HTTP/1.1 200 OK\r\n\r\n";
 
     let get = b"GET / HTTP/1.1\r\n"; // byte string
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        // simulate a slow request
+        thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK", "hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND", "404.html")
